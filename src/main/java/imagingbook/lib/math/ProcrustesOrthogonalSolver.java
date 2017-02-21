707d26dbac8f3de351a565d2f410317ec407ff65
Wilhelm Burger <wilbur@ieee.org>
2017-02-21 22:04:05 +0100

    a

--- src/main/java/imagingbook/lib/math/ProcrustesOrthogonalSolver.java
+++ src/main/java/imagingbook/lib/math/ProcrustesOrthogonalSolver.java
@@ -45,10 +45,13 @@ public class ProcrustesOrthogonalSolver {
 		RealMatrix BA = B.multiply(A.transpose());
 		SingularValueDecomposition svd = new SingularValueDecomposition(BA);
 		RealMatrix U = svd.getU();
+		RealMatrix S = svd.getS();
 		RealMatrix V = svd.getV();
-//		System.out.println("U = " + U.toString());
-//		System.out.println("V = " + V.toString());
-//		System.out.println("S = " + svd.getS().toString());
+		System.out.println("------ SVD ---------------");
+		System.out.println("U = " + Matrix.toString(U.getData()));
+		System.out.println("S = " + Matrix.toString(S.getData()));
+		System.out.println("V = " + Matrix.toString(V.getData()));
+		System.out.println("--------------------------");
 		RealMatrix Q = U.multiply(V.transpose());
 		return Q;
 	}
@@ -178,17 +181,96 @@ public class ProcrustesOrthogonalSolver {
 		System.out.println("error2 = " + fn*fn);
 		
 		double det = new LUDecomposition(Q).getDeterminant();
-		System.out.println("det(Q) = " + det);
-		
-//		RealMatrix Q2 = Q.scalarMultiply(3);
-//		double det2 = new LUDecomposition(Q2).getDeterminant();
-//		System.out.println("det(Q2) = " + det2);	
+		System.out.println("det(Q) = " + det);	
 	}
 	
-	public static void main(String[] args) {
-		PrintPrecision.set(3);
-		example1();
-		example2();
+	
+	static void example3() {	// orthogonal (rotation only)
+		System.out.println("************* Example 3 (rotation + translation + scaling) ****************");
+		double alpha = 0.6;	// rotation angle
+		System.out.println("original alpha = " + alpha);
+		
+		double scale = 3.5;
+		System.out.println("original scale = " + scale);
+		
+		
+		double[][] Q0 =
+			{{ Math.cos(alpha), -Math.sin(alpha) },
+			 { Math.sin(alpha),  Math.cos(alpha) }};
+		
+		double[] t0 = {4, -3};
+		
+		RealMatrix R = MatrixUtils.createRealMatrix(Q0);
+		System.out.println("original Q0 = \n" + Matrix.toString(R.getData()));
+		
+		List<double[]> lA = new ArrayList<double[]>();
+		List<double[]> lB = new ArrayList<double[]>();
+		
+		lA.add(new double[] {2, 5});
+		lA.add(new double[] {7, 3});
+		lA.add(new double[] {0, 9});
+		lA.add(new double[] {5, 4});
+		
+		for (double[] a : lA) {
+			double[] b = R.operate(a);
+			b[0] = roundToDigits(scale * b[0] + t0[0], NDIGITS);
+			b[1] = roundToDigits(scale * b[1] + t0[1], NDIGITS);
+			lB.add(b);
+		}
+		
+		// ----- solution ------
+		
+		RealMatrix A = makeMatrix(lA);
+		RealMatrix B = makeMatrix(lB);
+		System.out.println("A = \n" + Matrix.toString(A.getData()));
+		System.out.println("B = \n" + Matrix.toString(B.getData()));
+		
+		RealVector Amean = getMeanColumnVector(A);
+		RealVector Bmean = getMeanColumnVector(B);
+		
+		System.out.println("Amean = \n" + Matrix.toString(Amean.toArray()));
+		System.out.println("Bmean = \n" + Matrix.toString(Bmean.toArray()));
+		
+		// make A'
+		RealMatrix Ap = subtractColumnVector(A, Amean);
+		System.out.println("Ap = \n" + Matrix.toString(Ap.getData()));
+		
+		// make B'
+		RealMatrix Bp = subtractColumnVector(B, Bmean);
+		System.out.println("Bp = \n" + Matrix.toString(Bp.getData()));
+		
+		// calculate common scale factor
+//		double s = getScale(Ap, Bp);
+		double s = Bp.getFrobeniusNorm() / Ap.getFrobeniusNorm();
+		System.out.println("s = " + s); 
+		
+		RealMatrix Q = solve(Ap, Bp);
+		System.out.println("Q = \n" + Matrix.toString(Q.getData()));
+		
+		RealMatrix sQ = Q.scalarMultiply(s);
+		
+		RealVector t = Bmean.subtract(sQ.operate(Amean));
+		System.out.println("t = \n" + Matrix.toString(t.toArray()));
+		
+		// check if Q is orthogonal
+		RealMatrix II = Q.transpose().multiply(Q);
+		System.out.println("Q^T * Q = \n" + Matrix.toString(II.getData()));
+		
+		// calculate residual error
+		double err = 0;
+		for (int i = 0; i < lA.size(); i++) {
+			RealVector ai = new ArrayRealVector(lA.get(i));
+			RealVector bi = new ArrayRealVector(lB.get(i));
+			double ei = sQ.operate(ai).add(t).subtract(bi).getNorm();
+			err = err + ei * ei;
+		}
+		System.out.println("error1 = " + err);
+		
+//		double fn = Q.multiply(Ap).subtract(Bp).getFrobeniusNorm();
+//		System.out.println("error2 = " + fn*fn);
+		
+		double det = new LUDecomposition(Q).getDeterminant();
+		System.out.println("det(Q) = " + det);
 		
 	}
 
@@ -235,9 +317,34 @@ public class ProcrustesOrthogonalSolver {
 		return MM;
 	}
 	
+	static double getScale(RealMatrix A, RealMatrix B) {
+		double[][] a = A.getData();
+		double[][] b = B.getData();
+		int m = Matrix.getNumberOfColumns(a);
+		int n = Matrix.getNumberOfRows(a);
+//		System.out.println("    m = " + a[0].length);
+//		System.out.println("    n = " + a.length);
+		double sumA = 0;
+		double sumB = 0;
+		for (int r = 0; r < n; r++) {
+			for (int c = 0; c < m; c++) {
+				sumA = sumA + a[r][c] * a[r][c];
+				sumB = sumB + b[r][c] * b[r][c];
+			}
+		}
+		return Math.sqrt(sumB / sumA);
+	}
+	
 	static double roundToDigits(double x, int k) {
 		int d = (int) Math.pow(10, k);
 		return Math.rint(x * d) / d;
+	}
+	
+	public static void main(String[] args) {
+		PrintPrecision.set(10);
+//		example1();
+//		example2();
+		example3();
 	}
 
 }
