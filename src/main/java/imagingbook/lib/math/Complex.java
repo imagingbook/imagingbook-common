b4a740bc5677a8ec832fe366b2cc15bd6cc4d522
imagingbook <wilhelm.burger@fh-hagenberg.at>
2018-03-18 12:35:27 +0100

    substantial refactoring of class Complex (to align with Apache Commons implementation)

--- src/main/java/imagingbook/lib/math/Complex.java
+++ src/main/java/imagingbook/lib/math/Complex.java
@@ -3,190 +3,189 @@
  *  image processing published by Springer-Verlag in various languages and editions.
  * Permission to use and distribute this software is granted under the BSD 2-Clause 
  * "Simplified" License (see http://opensource.org/licenses/BSD-2-Clause). 
- * Copyright (c) 2006-2016 Wilhelm Burger, Mark J. Burge. All rights reserved. 
+ * Copyright (c) 2006-2018 Wilhelm Burger, Mark J. Burge. All rights reserved. 
  * Visit http://imagingbook.com for additional details.
  *******************************************************************************/
 
 package imagingbook.lib.math;
 
-import ij.IJ;
-import imagingbook.lib.math.Complex;
-
-import java.awt.geom.Point2D;
-import java.util.Locale;
-
+/**
+ * This class represents complex numbers. Instances are immutable.
+ * Methods are mostly defined to be compatible with 
+ * org.apache.commons.math3.complex.Complex and (newer)
+ * org.apache.commons.numbers.complex.Complex.
+ * Arithmetic operations are generally more accurate than with the
+ * Apache implementation.
+ * 
+ * @author W. Burger
+ * @version 2018/03/18
+ */
 public class Complex {
-
-	private final double re;
-	private final double im;
 	
-	public double re() {
-		return this.re;
-	}
-	
-	public double im() {
-		return this.im;
-	}
+	public static final Complex ONE = new Complex(1, 0);
+	public static final Complex ZERO = new Complex(0, 0);
+	public static final Complex I = new Complex(0, 1);
 
+	/** The real part of this complex number */
+	public final double re;
+	/** The imaginary part of this complex number */
+	public final double im;
+
+	/**
+	 * Constructor.
+	 * @param re real part
+	 * @param im imaginary part
+	 */
 	public Complex(double re, double im) {
 		this.re = re;
 		this.im = im;
 	}
 
-	public Complex(Point2D p) {
-		this.re = p.getX();
-		this.im = p.getY();
+	/**
+	 * Constructor
+	 * @param z complex quantity, which is duplicated
+	 */
+	public Complex(Complex z) {
+		this.re = z.re;
+		this.im = z.im;
 	}
 
-	public Complex(Complex c) {
-		this.re = c.re;
-		this.im = c.im;
-	}
 
-	/*
-	 * Create a complex quantity on the unit circle with angle 'phi'.
+	/**
+	 * Create a complex quantity on the unit circle with angle 'phi':
 	 * e^{\i \phi} = \cos(\phi) + \i \cdot \sin(\phi)
+	 * @param phi angle
 	 */
 	public Complex(double phi) {
 		this.re = Math.cos(phi);
 		this.im = Math.sin(phi);
 	}
 
-	public static Complex[] makeComplexVector(int M) {
-		Complex[] g = new Complex[M];
-		for (int i = 0; i < M; i++) {
-			g[i] = new Complex(0, 0);
-		}
-		return g;
-	}
-
-	public static Complex[] duplicate(Complex[] g1) {
-		Complex[] g2 = new Complex[g1.length];
-		for (int i = 0; i < g1.length; i++) {
-			g2[i] = new Complex(g1[i].re, g1[i].im);
-		}
-		return g2;
-	}
-
-
-	public static Complex[] makeComplexVector(double[] signal) {
-		int M = signal.length;
-		Complex[] g = new Complex[M];
-		for (int i = 0; i < M; i++) {
-			g[i] = new Complex(signal[i], 0);
-		}
-		return g;
-	}
-
-	public static Complex[] makeComplexVector(double[] real, double[] imag) {
-		int M = real.length;
-		Complex[] g = new Complex[M];
-		for (int i = 0; i < M; i++) {
-			g[i] = new Complex(real[i], imag[i]);
-		}
-		return g;
-	}
-
-	// radius
+	/**
+	 * Returns the absolute value of this complex number, i.e.,
+	 * its radius or distance from the origin.
+	 * @return the absolute value
+	 */
 	public double abs() {
 		return Math.sqrt(this.abs2());
 	}
 
-	// squared radius
+	/**
+	 * Returns the squared absolute value of this complex number, i.e.,
+	 * its squared radius or distance from the origin.
+	 * @return the squared absolute value
+	 */
 	public double abs2() {
 		return re * re + im * im;
 	}
 
-	// angle 
+	/**
+	 * Returns the 'argument' of this complex number, i.e.,
+	 * its angle relative to the real axis. 
+	 * @return the argument (in radians)
+	 */
 	public double arg() {
 		return Math.atan2(im, re);
 	}
 
-
-	/*
-	 *  ---- complex arithmetic ---------------
-	 */
-
-	// complex conjugate
+	/**
+     * Returns the conjugate {@code z*} of this complex number, i.e,
+     * if {@code z = a + i b} then {@code z* = a - i b}.
+     *
+     * @return the complex conjugate
+     */
 	public Complex conjugate() {
 		return new Complex(this.re, -this.im);
 	}
 
-	public Complex add(Complex c2) {
-		return new Complex(this.re + c2.re,  this.im + c2.im);
+	/**
+	 * Adds a complex quantity to this complex number and returns
+	 * a new complex number.
+	 * @param z
+	 * @return the sum of this complex number and {@code z}
+	 */
+	public Complex add(Complex z) {
+		return new Complex(this.re + z.re,  this.im + z.im);
 	}
 
-	public Complex mult(double s) {
-		return new Complex(this.re * s, this.im * s);
-	}
-
-	public Complex mult(Complex c2) {
-		// (x1 + i y1)(x2 + i y2) = (x1 x2 + y1 y2) + i (x1 y2 + y1 x2)
-		Complex c1 = this;
-		double x = c1.re * c2.re - c1.im * c2.im;
-		double y = c1.re * c2.im + c1.im * c2.re;
-		return new Complex(x, y);
-	}
-
+	/**
+	 * Rotates this complex number by the angle {@code phi} and returns
+	 * a new complex number.
+	 * @param phi the angle
+	 */
 	public Complex rotate(double phi) {
-		return this.mult(new Complex(phi));
+		return this.multiply(new Complex(phi));
 	}
 
-	public double distance(Complex c2) {
-		Complex c1 = this;
-		double dRe = c1.re - c2.re;
-		double dIm = c1.im - c2.im;
-		return Math.sqrt(dRe*dRe + dIm*dIm);
-	}
-
-	public double crossProduct(Complex c2) {
-		Complex c1 = this;
-		return c1.re * c2.im - c1.im * c2.re;
-	}
-
-	public double dotProduct(Complex c2) {
-		Complex c1 = this;
-		return c1.re * c2.re + c1.im * c2.im;
+	@Override
+    public String toString() {
+        return "(" + this.re + ", " + this.im + ")";
+    }
+	
+    /**
+     * Returns true if the real or imaginary component of this complex number
+     * is {@code NaN}.
+     * @return true if {@code NaN}, otherwise false
+     */
+	public boolean isNaN() {
+		return Double.isNaN(this.getReal()) || Double.isNaN(this.getImaginary());
 	}
 	
-	public Complex exp(int k) {
+	/**
+	 * Returns the real part of this complex number.
+	 * @see {@link #re} (public member variable)
+	 * @return the real part
+	 */
+	public double getReal() {
+		return this.re;
+	}
+	
+	/**
+	 * Returns the imaginary part of this complex number.
+	 * @see {@link #im} (public member variable)
+	 * @return the imaginary part
+	 */
+	public double getImaginary() {
+		return this.im;
+	}
+
+	/**
+	 * Multiplies this complex number with another complex quantity {@code z2} and returns
+	 * a new complex number.
+	 * @param z2 a complex quantity
+	 * @return this complex number multiplied by {@code z2}
+	 */
+	public Complex multiply(Complex z2) {
+		// (x1 + i y1)(x2 + i y2) = (x1 x2 + y1 y2) + i (x1 y2 + y1 x2)
+		Complex z1 = this;
+		double x = z1.re * z2.re - z1.im * z2.im;
+		double y = z1.re * z2.im + z1.im * z2.re;
+		return new Complex(x, y);
+	}
+	
+	/**
+	 * Multiplies this complex number with the scalar factor {@code s} and returns
+	 * a new complex number.
+	 * @param s a scalar factor
+	 * @return this complex number multiplied by {@code s}
+	 */
+	public Complex multiply(double s) {
+		return new Complex(this.re * s, this.im * s);
+	}
+	
+	 /**
+     * Returns of value of this complex number raised to the power of {@code k}.
+     * @param k the exponent
+     * @return {@code z^k}
+     */
+	public Complex pow(int k) {
 		if (k < 0) throw new IllegalArgumentException("exponent k >= 0 expected");
 		Complex prod = new Complex(1, 0);
 		for (int i = 0; i < k; i++) {
-			prod = prod.mult(this);
+			prod = prod.multiply(this);
 		}
 		return prod;
 	}
-
-	public String toString() {
-		if (this.im >= 0) {
-			return String.format(Locale.US, "(%.4f + %.4f i)", this.re, this.im);
-		}
-		else {
-			return String.format(Locale.US, "(%.4f - %.4f i)", this.re, Math.abs(this.im));
-		}
-	}
-	
-	// -------------------------------------------------------
-	
-	public static void printComplexVector(Complex[] g, String title) {
-		IJ.log("Listing of " + title);
-		for (int i = 0; i < g.length; i++) {
-			if (g[i] == null)
-				IJ.log(String.format("%d: ********", i));
-			else {
-				double gr = g[i].re;
-				double gi = g[i].im;
-				if (gi >= 0) {
-					IJ.log(String.format(Locale.US, "%d: %6.2f + %6.2fi", i, gr, Math.abs(gi)));
-				}
-				else {
-					IJ.log(String.format(Locale.US, "%d: %6.2f - %6.2fi", i, gr, Math.abs(gi)));
-				}
-			}
-		}
-	}
-
 
 	//------------ TESTING only ------------------------------
 
@@ -195,9 +194,9 @@ public class Complex {
 		Complex z2 = new Complex(-1, 0.2);
 		System.out.println("z1 = " + z1);
 		System.out.println("z2 = " + z2);
-		Complex z3 = z1.mult(z2);
+		Complex z3 = z1.multiply(z2);
 		System.out.println("z3 = " + z3);
-		Complex z4 = z2.mult(z1);
+		Complex z4 = z2.multiply(z1);
 		System.out.println("z4 = " + z4);
 	}
 
