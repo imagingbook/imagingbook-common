b7fa35c35c9c85190c13fa756bc6ef1f179d7876
imagingbook <wilhelm.burger@fh-hagenberg.at>
2018-03-31 11:42:46 +0200

    fixed Mod() bug in Arithmetic.java

--- src/main/java/imagingbook/lib/math/Arithmetic.java
+++ src/main/java/imagingbook/lib/math/Arithmetic.java
@@ -28,22 +28,34 @@ public abstract class Arithmetic {
 	}
 	
 	/**
-	 * Integer version of modulus operator (as described in the book).
-	 * Calculates {@code a mod b}.
-	 * * Also see <a href="http://en.wikipedia.org/wiki/Modulo_operation">here</a>.
-	 * @param a dividend
-	 * @param b divisor
+	 * Integer version of the modulus operator ({@code a mod b}).
+	 * Also see <a href="http://en.wikipedia.org/wiki/Modulo_operation">here</a>.
+	 * This implementation has the same behavior as the {@code Math.floorMod(a,b)}
+	 * in Java 8 and higher (does not allow {@code b!=0}, however).
+	 * @param a the dividend
+	 * @param b the divisor
 	 * @return {@code a mod b}
 	 */
 	public static int mod(int a, int b) {
 		if (b == 0)
 			return a;
-		if (a * b >= 0)	// a, b are either both positive or negative
-			return a - b * (a / b);	
-		else
-			return a - b * (a / b - 1);
-//		return Math.floorMod(a, a); 	// equivalent, but requires Java 1.8
+		// a, b have the same sign OR the remainder is zero
+        if ((long)a * b >= 0 || a % b == 0)	// or (a / b) * b == a
+        	return a - b * (a / b);
+        else
+        	return a - b * (a / b - 1);
 	}
+	
+	//  Original (published) method (gives wrong results for negative a):
+	//	public static int mod(int a, int b) {
+	//		if (b == 0)
+	//			return a;
+	//		if (a * b > 0)	// a, b are either both positive or negative
+	//			return a - b * (a / b);	
+	//		else
+	//			return a - b * (a / b - 1);
+	//	}
+
 	
 	/**
 	 * Non-integer version of modulus operator, with results identical to Mathematica. 
@@ -79,10 +91,15 @@ public abstract class Arithmetic {
 	// -------------------------------------
 	
 	public static void main(String[] args) {
-		System.out.println(Arithmetic.mod(4, 3));
-		System.out.println(Arithmetic.mod(4, -3));
-		System.out.println(Arithmetic.mod(-4, 3));
-		System.out.println(Arithmetic.mod(-4, -3));
+		System.out.println(Arithmetic.mod(13, 4));
+		System.out.println(Arithmetic.mod(13, -4));
+		System.out.println(Arithmetic.mod(-13, 4));
+		System.out.println(Arithmetic.mod(-13, -4));
+		
+		int b = 7;
+		for (int i = -25; i < 25; i++) {
+			System.out.format("%4d  -> %4d  | %4d \n", i, Math.floorMod(i, b), Arithmetic.mod(i, b));
+		}
 	}
 
 }
