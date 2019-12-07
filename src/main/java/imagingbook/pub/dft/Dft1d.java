e506ef506341d9468761f279d60026e78b118d79
imagingbook <wilhelm.burger@fh-hagenberg.at>
2019-12-07 19:15:52 +0100

    fixing DFT

--- src/main/java/imagingbook/pub/dft/Dft1d.java
+++ src/main/java/imagingbook/pub/dft/Dft1d.java
@@ -9,7 +9,11 @@
 
 package imagingbook.pub.dft;
 
+import java.util.AbstractMap;
+import java.util.Arrays;
+
 import imagingbook.lib.math.Complex;
+import imagingbook.lib.math.Matrix;
 
 /**
  * One-dimensional Discrete Fourier Transform (no FFT but using
@@ -21,13 +25,19 @@ import imagingbook.lib.math.Complex;
 public class Dft1d {
 
 	private final int M;
+	
 	private final double[] cosTable;
 	private final double[] sinTable;
+	
+	private final double[] GRe;
+	private final double[] GIm;
 	
 	Dft1d(int M) {
 		this.M = M;
 		this.cosTable = makeCosTable();
 		this.sinTable = makeSinTable();
+		GRe = new double[M];
+		GIm = new double[M];
 	}
 	
 	double[] makeCosTable() {
@@ -80,68 +90,85 @@ public class Dft1d {
 	}
 	
 	/**
-	 * Forward DFT applied to a complex-valued input signal.
-	 * @param g	complex-valued 1D input signal: g[i][0] = Re, g[i][1] = Im 
-	 * @return a complex-valued spectrum (forward) or reconstructed signal (inverse)
+	 * Forward DFT applied to a complex-valued input signal (forward = true)
+	 * or inverse DFT applied to a complex-values spectrum (forward = false).
+	 * The transformation operates "in place", i.e., the two input arrays
+	 * are modified.
+	 * 
+	 * @param gRe	real part of the signal	(must be of length M)
+	 * @param gIm	imaginary part of the signal (must be of length M)
+	 * @param forward
 	 */
-	public double[][] dft(double[][] g, boolean forward) {
-		if (M != g.length) {
-			throw new IllegalArgumentException(String.format("Dft1d: length of input signal g (%d) must be %d", g.length, M));
+	public void dft(double[] gRe, double[] gIm, boolean forward) {
+		if (M != gRe.length || M != gIm.length) {	// TODO: add more checks!
+			throw new IllegalArgumentException(String.format("Dft1d: length of input signal g (%d) must be %d", gRe.length, M));
 		}
-		double[][] G = new double[M][2];
 		double s = 1.0 / Math.sqrt(M); //common scale factor
 		for (int u = 0; u < M; u++) {
 			double sumRe = 0;
 			double sumIm = 0;
 			for (int m = 0; m < M; m++) {
-				double gRe = g[m][0];
-				double gIm = g[m][1];
+				double re = gRe[m];
+				double im = (gIm == null) ? 0 : gIm[m];
 				int k = (u * m) % M;
 				double cosPhi = cosTable[k];
 				double sinPhi = sinTable[k];
 				if (forward)
 					sinPhi = -sinPhi;
 				//complex multiplication: (gRe + i gIm) * (cosPhi + i sinPhi)
-				sumRe += gRe * cosPhi - gIm * sinPhi;
-				sumIm += gRe * sinPhi + gIm * cosPhi;
+				sumRe += re * cosPhi - im * sinPhi;
+				sumIm += re * sinPhi + im * cosPhi;
 			}
-			G[u][0] = s * sumRe;	
-			G[u][1] = s * sumIm;
+			GRe[u] = s * sumRe;	
+			GIm[u] = s * sumIm;
 		}
-		return G;
+		// copy the results back the input arrays
+		System.arraycopy(GRe, 0, gRe, 0, gRe.length);
+		System.arraycopy(GIm, 0, gIm, 0, gIm.length);
 	}
 	
-	/**
-	 * Forward/inverse DFT applied to a real-valued input signal.
-	 * @param g	real-valued 1D input signal: g[i] = Re, 
-	 * @param forward true: forward DFT, false: inverse DFT
-	 * @return a complex-valued spectrum (forward) or reconstructed signal (inverse)
-	 */
-	public double[][] dft(double[] g) {
-		if (M != g.length) {
-			throw new IllegalArgumentException(String.format("Dft1d: length of input signal g (%d) must be %d", g.length, M));
-		}
-		final boolean forward = true;
-		double[][] G = new double[M][2];
-		double s = 1.0 / Math.sqrt(M); //common scale factor
-		for (int u = 0; u < M; u++) {
-			double sumRe = 0;
-			double sumIm = 0;
-			for (int m = 0; m < M; m++) {
-				double gRe = g[m];
-				double gIm = 0;
-				int k = (u * m) % M;
-				double cosPhi = cosTable[k];
-				double sinPhi = sinTable[k];
-				if (forward)
-					sinPhi = -sinPhi;
-				//complex multiplication: (gRe + i gIm) * (cosPhi + i sinPhi)
-				sumRe += gRe * cosPhi - gIm * sinPhi;
-				sumIm += gRe * sinPhi + gIm * cosPhi;
-			}
-			G[u][0] = s * sumRe;	
-			G[u][1] = s * sumIm;
-		}
-		return G;
+	
+	// ------------------------------------------------------------------------------
+	
+	/*
+	 * Direct implementation of the one-dimensional DFT for arbitrary signal lengths.
+	 * This DFT uses the same definition as Mathematica. Example:
+	 * Fourier[{1, 2, 3, 4, 5, 6, 7, 8}, FourierParameters = {0, -1}]:
+		{12.7279 + 0. i, 
+		-1.41421 + 3.41421 i, 
+		-1.41421 + 1.41421 i, 
+		-1.41421 + 0.585786 i, 
+		-1.41421 + 0. i, 
+		-1.41421 - 0.585786 i, 
+		-1.41421 - 1.41421 i, 
+		-1.41421 - 3.41421 i}
+	*/
+	
+	//test example
+	public static void main(String[] args) {
+		double[] gRe = { 1, 2, 3, 4, 5, 6, 7, 8 };
+		double[] gIm = new double[gRe.length];
+		
+		System.out.println("original signal:");
+		System.out.println("gRe = " + Matrix.toString(gRe));
+		System.out.println("gIm = " + Matrix.toString(gIm));
+		
+		Dft1d dft1 = new Dft1d(gRe.length);
+		dft1.dft(gRe, gIm, true);
+		
+		System.out.println("DFT spectrum:");
+		System.out.println("GRe = " + Matrix.toString(gRe));
+		System.out.println("GIm = " + Matrix.toString(gIm));
+		
+		dft1.dft(gRe, gIm, false);
+		
+		System.out.println("reconstructed signal:");
+		System.out.println("gRe = " + Matrix.toString(gRe));
+		System.out.println("gIm = " + Matrix.toString(gIm));
+
+		
 	}
+	
+	
+	
 }
--- src/main/java/imagingbook/pub/dft/Dft2d.java
+++ src/main/java/imagingbook/pub/dft/Dft2d.java
@@ -9,6 +9,8 @@
 
 package imagingbook.pub.dft;
 
+import java.util.Arrays;
+
 import ij.process.*;
 import imagingbook.lib.math.Complex;
 import imagingbook.pub.dft.Dft1d;
@@ -25,29 +27,23 @@ import imagingbook.pub.dft.Dft1d;
  */
 public class Dft2d {
 	
-	private final int width, height;
+//	private final int width, height;
 	
-	private final double[][] row;	// complex-valued image row
-	private final double[][] col;	// complex-valued image column
+//	private final double[][] row;	// complex-valued image row
+//	private final double[][] col;	// complex-valued image column
 	
-	private final Dft1d dftR;
-	private final Dft1d dftC;
+//	private final Dft1d dftR;
+//	private final Dft1d dftC;
+//	
+//	private float[] Real;	//original image data
+//	private float[] Imag;
+//	private float[] Power;
+//	private float PowerMax;
+//	private boolean swapQu = true;
+//	private int scaleValue = 255;	
+//	private boolean forward = true;
 	
-	private float[] Real;	//original image data
-	private float[] Imag;
-	private float[] Power;
-	private float PowerMax;
-	private boolean swapQu = true;
-	private int scaleValue = 255;	
-	private boolean forward = true;
-	
-	public Dft2d(int width, int height) {
-		this.width = width;
-		this.height = height;
-		this.row = new double[width][2];
-		this.col = new double[height][2];
-		this.dftR = new Dft1d(width);
-		this.dftC = new Dft1d(height);
+	public Dft2d() {
 	}
 	
 	//------------------------------------------------
@@ -57,201 +53,213 @@ public class Dft2d {
 	 * @param g	2D complex-valued input signal: g[u][v][0] = re, g[u][v][1] = im
 	 * @return
 	 */
-	public double[][][] dft(double[][][] g) {
-		if (width != g.length || height != g[0].length) {
-			throw new IllegalArgumentException(String.format("Dft2d: size of input signal g (%d,%d) must be (%d,%d)", 
-					g.length, g[0].length, width, height));
-		}
-		// do the rows:
-		//Complex[] row = makeComplexVector(width);
-		//Dft1d dftR = new Dft1d(width);
+	public void dft(double[][] gRe, double[][] gIm, boolean forward) {
+		final int width = gRe.length;
+		final int height = gRe[0].length;
+
+		// transform each row (in place):
+		final double[] rowRe = new double[width];
+		final double[] rowIm = new double[width];
+		Dft1d dft1Row = new Dft1d(width);
 		for (int v = 0; v < height; v++) {
-			getRow(g, v, row);
-			dftR.dft(row, forward);
-			putRow(g, v, row);
+			getRow(gRe, v, rowRe);
+			getRow(gIm, v, rowIm);
+			dft1Row.dft(rowRe, rowIm, forward);
+			putRow(gRe, v, rowRe);
+			putRow(gIm, v, rowIm);
 		}
-		// do the columns:
-		//Complex[] col = makeComplexVector(height);
-		//Dft1d dftC = new Dft1d(height);
+		
+		// transform each column (in place):
+		final double[] colRe = new double[height];
+		final double[] colIm = new double[height];
+		Dft1d dftCol = new Dft1d(height);
 		for (int u = 0; u < width; u++) {
-			getCol(g, u, col);
-			dftR.dft(col, forward);
-			putCol(g, u, col);
+			getCol(gRe, u, colRe);
+			getCol(gIm, u, colIm);
+			dftCol.dft(colRe, colIm, forward);
+			putCol(gRe, u, colRe);
+			putCol(gIm, u, colIm);
 		}
-		return null;
 	}
+	
+	//------------------------------------------------
 	
 	// copy the values of row 'v' of 'g' into 'row'
-	private void getRow(double[][][] g, final int v, double[][] row) {
-		for (int u = 0; u < width; u++) {
-			row[u][0] = g[u][v][0];
-			row[u][1] = g[u][v][1];
+	private void getRow(double[][] g, final int v, double[] row) {
+		if (g == null) {
+			Arrays.fill(row, 0);
+		}
+		else {
+			for (int u = 0; u < row.length; u++) {
+				row[u] = g[u][v];
+			}
 		}
 	}
 	
-	void putRow(double[][][] g, int v, double[][] row) {
-		for (int u = 0; u < width; u++) {
-			g[u][v][0] = row[u][0];
-			g[u][v][1] = row[u][1];
+	void putRow(double[][] g, int v, double[] row) {
+		for (int u = 0; u < row.length; u++) {
+			g[u][v] = row[u];
 		}
 	}
 	
 	// copy the values of column 'u' of 'g' into 'cols'
-	private void getCol(double[][][] g, final int u, double[][] col) {
-		for (int v = 0; v < height; v++) {
-			col[v][0] = g[u][v][0];
-			col[v][1] = g[u][v][1];
+	private void getCol(double[][] g, final int u, double[] col) {
+		if (g == null) {
+			Arrays.fill(col, 0);
+		}
+		else {
+			for (int v = 0; v < col.length; v++) {
+				col[v] = g[u][v];
+			}
 		}
 	}
 	
-	private void putCol(double[][][] g, final int u, double[][] col) {
-		for (int v = 0; v < height; v++) {
-			g[u][v][0] = col[v][0];
-			g[u][v][1] = col[v][1];
-		}
-	}
-	
-	
-	//------------------------------------------------
-	//------------------------------------------------
-	
-	@Deprecated
-	public Dft2d(FloatProcessor ip) {
-		this(ip.getWidth(), ip.getHeight());
-		Real = (float[]) ip.getPixels();
-		Imag = new float[width*height];  // values are zero
-		doDft2d();
-		makePowerSpectrum();
-	}
-	
-	@Deprecated
-	public Dft2d(FloatProcessor ip, boolean center){
-		this(ip);
-		swapQu = center;
-	}
-	
-	//------------------------------------------------
-	
-	@Deprecated
-	public float[] getReal() {
-		return Real;
-	}
-	
-	@Deprecated
-	public float[] getImag(){
-		return Imag;
-	}
-	
-	@Deprecated
-	public float[] getPower(){
-		return Power;
-	}
-	
-	//------------------------------------------------
-	
-	@Deprecated
-	public void doDft2d() { // in-place 2D Dft
-		// do the rows:
-		Complex[] row = makeComplexVector(width);
-		//Dft1d dftR = new Dft1d(width);
-		for (int v = 0; v < height; v++) {
-			getRow(v, row);
-			Complex[] rowDft = dftR.dft(row, forward);
-			putRow(v, rowDft);
-		}
-		// do the columns:
-		Complex[] col = makeComplexVector(height);
-		//Dft1d dftC = new Dft1d(height);
-		for (int u = 0; u < width; u++) {
-			getCol(u, col);
-			Complex[] colDft = dftC.dft(col, forward);
-			putCol(u, colDft);
+	private void putCol(double[][] g, final int u, double[] col) {
+		for (int v = 0; v < col.length; v++) {
+			g[u][v] = col[v];
 		}
 	}
 	
 	//------------------------------------------------
+	//------------------------------------------------
 	
-	@Deprecated
-	private void getRow(int v, Complex[] rowC) {
-		int i = v * width; // start index of row v
-		for (int u = 0; u < width; u++) {
-//			rowC[u].re = Real[i+u];
-//			rowC[u].im = Imag[i+u];
-			rowC[u] = new Complex(Real[i + u], Imag[i + u]);
-		}
-	}
+//	@Deprecated
+//	public Dft2d(FloatProcessor ip) {
+//		this(ip.getWidth(), ip.getHeight());
+//		Real = (float[]) ip.getPixels();
+//		Imag = new float[width*height];  // values are zero
+//		doDft2d();
+//		makePowerSpectrum();
+//	}
+//	
+//	@Deprecated
+//	public Dft2d(FloatProcessor ip, boolean center){
+//		this(ip);
+//		swapQu = center;
+//	}
+//	
+//	//------------------------------------------------
+//	
+//	@Deprecated
+//	public float[] getReal() {
+//		return Real;
+//	}
+//	
+//	@Deprecated
+//	public float[] getImag(){
+//		return Imag;
+//	}
+//	
+//	@Deprecated
+//	public float[] getPower(){
+//		return Power;
+//	}
+//	
+	//------------------------------------------------
 	
-	@Deprecated
-	void putRow(int v, Complex[] rowC) {
-		int i = v * width; // start index of row v
-		for (int u = 0; u < width; u++) {
-			Real[i + u] = (float) rowC[u].re;
-			Imag[i + u] = (float) rowC[u].im;
-		}
-	}
+//	@Deprecated
+//	public void doDft2d() { // in-place 2D Dft
+//		// do the rows:
+//		Complex[] row = makeComplexVector(width);
+//		//Dft1d dftR = new Dft1d(width);
+//		for (int v = 0; v < height; v++) {
+//			getRow(v, row);
+//			Complex[] rowDft = dftR.dft(row, forward);
+//			putRow(v, rowDft);
+//		}
+//		// do the columns:
+//		Complex[] col = makeComplexVector(height);
+//		//Dft1d dftC = new Dft1d(height);
+//		for (int u = 0; u < width; u++) {
+//			getCol(u, col);
+//			Complex[] colDft = dftC.dft(col, forward);
+//			putCol(u, colDft);
+//		}
+//	}
+//	
+	//------------------------------------------------
 	
-	@Deprecated
-	void getCol(int u, Complex[] rowC) {
-		for (int v = 0; v < height; v++) {
-//			rowC[v].re = Real[v*width+u];
-//			rowC[v].im = Imag[v*width+u];
-			rowC[v] = new Complex(Real[v * width + u], Imag[v * width + u]);
-		}
-	}
-	
-	@Deprecated
-	void putCol(int u, Complex[] rowC) {
-		for (int v = 0; v < height; v++) {
-			Real[v * width + u] = (float) rowC[v].re;
-			Imag[v * width + u] = (float) rowC[v].im;
-		}
-	}
-	
-	Complex[] makeComplexVector(int M) {
-		Complex[] g = new Complex[M];
-		for (int i = 0; i < M; i++) {
-			g[i] = new Complex(0, 0);
-		}
-		return g;
-	}
+//	@Deprecated
+//	private void getRow(int v, Complex[] rowC) {
+//		int i = v * width; // start index of row v
+//		for (int u = 0; u < width; u++) {
+////			rowC[u].re = Real[i+u];
+////			rowC[u].im = Imag[i+u];
+//			rowC[u] = new Complex(Real[i + u], Imag[i + u]);
+//		}
+//	}
+//	
+//	@Deprecated
+//	void putRow(int v, Complex[] rowC) {
+//		int i = v * width; // start index of row v
+//		for (int u = 0; u < width; u++) {
+//			Real[i + u] = (float) rowC[u].re;
+//			Imag[i + u] = (float) rowC[u].im;
+//		}
+//	}
+//	
+//	@Deprecated
+//	void getCol(int u, Complex[] rowC) {
+//		for (int v = 0; v < height; v++) {
+////			rowC[v].re = Real[v*width+u];
+////			rowC[v].im = Imag[v*width+u];
+//			rowC[v] = new Complex(Real[v * width + u], Imag[v * width + u]);
+//		}
+//	}
+//	
+//	@Deprecated
+//	void putCol(int u, Complex[] rowC) {
+//		for (int v = 0; v < height; v++) {
+//			Real[v * width + u] = (float) rowC[v].re;
+//			Imag[v * width + u] = (float) rowC[v].im;
+//		}
+//	}
+//	
+//	Complex[] makeComplexVector(int M) {
+//		Complex[] g = new Complex[M];
+//		for (int i = 0; i < M; i++) {
+//			g[i] = new Complex(0, 0);
+//		}
+//		return g;
+//	}
 
 	
 	//----------------------------------------------------
 	
-	void makePowerSpectrum() {
-		// computes the power spectrum
-		Power = new float[Real.length];
-		PowerMax = 0.0f;
-		for (int i = 0; i < Real.length; i++) {
-			double a = Real[i];
-			double b = Imag[i];
-			float p = (float) Math.sqrt(a * a + b * b);
-			Power[i] = p;
-			if (p > PowerMax)
-				PowerMax = p;
-		}
-	}
-	
-	public ByteProcessor makePowerImage() {
-		ByteProcessor ip = new ByteProcessor(width, height);
-		byte[] pixels = (byte[]) ip.getPixels();
-		// PowerMax must be set
-		double max = Math.log(PowerMax + 1.0);
-		double scale = 1.0;
-		if (scaleValue > 0)
-			scale = scaleValue / max;
-		for (int i = 0; i < pixels.length; i++) {
-			double p = Power[i];
-			if (p < 0)
-				p = -p;
-			double plog = Math.log(p + 1.0);
-			int pint = (int) (plog * scale);
-			pixels[i] = (byte) (0xFF & pint);
-		}
-		if (swapQu)
-			swapQuadrants(ip);
-		return ip;
-	}
+//	void makePowerSpectrum() {
+//		// computes the power spectrum
+//		Power = new float[Real.length];
+//		PowerMax = 0.0f;
+//		for (int i = 0; i < Real.length; i++) {
+//			double a = Real[i];
+//			double b = Imag[i];
+//			float p = (float) Math.sqrt(a * a + b * b);
+//			Power[i] = p;
+//			if (p > PowerMax)
+//				PowerMax = p;
+//		}
+//	}
+//	
+//	public ByteProcessor makePowerImage() {
+//		ByteProcessor ip = new ByteProcessor(width, height);
+//		byte[] pixels = (byte[]) ip.getPixels();
+//		// PowerMax must be set
+//		double max = Math.log(PowerMax + 1.0);
+//		double scale = 1.0;
+//		if (scaleValue > 0)
+//			scale = scaleValue / max;
+//		for (int i = 0; i < pixels.length; i++) {
+//			double p = Power[i];
+//			if (p < 0)
+//				p = -p;
+//			double plog = Math.log(p + 1.0);
+//			int pint = (int) (plog * scale);
+//			pixels[i] = (byte) (0xFF & pint);
+//		}
+//		if (swapQu)
+//			swapQuadrants(ip);
+//		return ip;
+//	}
 
 	//	----------------------------------------------------
 
