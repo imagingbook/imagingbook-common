90e9b70404ac3d196425f186490bcecf5d3b9023
imagingbook <wilhelm.burger@fh-hagenberg.at>
2020-02-09 09:23:42 +0100

    cosmetics

--- src/main/java/imagingbook/lib/math/Eigensolver2x2.java
+++ src/main/java/imagingbook/lib/math/Eigensolver2x2.java
@@ -11,30 +11,32 @@ package imagingbook.lib.math;
 
 
 import imagingbook.lib.math.Matrix;
-import imagingbook.lib.settings.PrintPrecision;
+//import imagingbook.lib.settings.PrintPrecision;
 
 /**
- * Instances of class calculate the real eigenvalues (&lambda;) and 
- * eigenvectors (x) for a 2x2 matrix M of the form
+ * Implements an efficient, closed form algorithm for calculating the real 
+ * eigenvalues (&lambda;) and eigenvectors (x) of a 2x2 matrix M of the form
  * <pre>
  *   | A B |
  *   | C D | </pre>
  * There are typically (but not in general) two pairs of real-valued 
- * solutions (&lambda;, x).
- * such that M*x = &lambda;*x.
+ * solutions 
+ * &lang;&lambda;<sub>1</sub>, x<sub>1</sub>&rang;,
+ * &lang;&lambda;<sub>2</sub>, x<sub>2</sub>&rang;
+ * such that M&middot;x<sub>k</sub> = &lambda;<sub>k</sub>&middot;x<sub>k</sub>.
  * <p>
  * This implementation is inspired by Blinn, Jim: "Jim Blinn's Corner: 
  * Notation, Notation, Notation", Morgan Kaufmann (2002) -
  * Ch. 5 ("Consider the Lowly 2x2 Matrix").
  * Note that Blinn uses the (common computer graphics) notation 
- * x*M = &lambda;*x,
+ * x&middot;M = &lambda;&middot;x,
  * while this implementation adopts the notation 
- * M*x = &lambda;*x, 
+ * M&middot;x = &lambda;&middot;x, 
  * i.e., the matrix M is transposed (elements B/C are exchanged).
  * </p>
  * 
  * @author W. Burger
- * @version 2020-02-08
+ * @version 2020-02-09
  */
 public class Eigensolver2x2 {
 	
@@ -42,10 +44,21 @@ public class Eigensolver2x2 {
 	private final double[] eVals = {Double.NaN, Double.NaN};
 	private final double[][] eVecs = new double[2][];
 	
+	/**
+	 * Constructor, takes a 2x2 matrix.
+	 * @param M a 2x2 matrix
+	 */
 	public Eigensolver2x2(double[][] M) {
 		this(M[0][0], M[0][1], M[1][0], M[1][1]);
 	}
 	
+	/**
+	 * Constructor, takes the individual elements of a 2x2 matrix.
+	 * @param A matrix element M[0,0]
+	 * @param B matrix element M[0,1]
+	 * @param C matrix element M[1,0]
+	 * @param D matrix element M[1,1]
+	 */
 	public Eigensolver2x2(double A, double B, double C, double D) {
 		this.isReal = makeEigenvectors(A, B, C, D);
 	}
@@ -53,12 +66,15 @@ public class Eigensolver2x2 {
 	private boolean makeEigenvectors(double A, double B, double C, double D) {
 		final double R = (A + D) / 2;
 		final double S = (A - D) / 2;
-		final double T = Math.sqrt(S * S + B * C);	// = sqrt(rho) = NaN if negative argument
-		eVals[0] = R + T;
-		eVals[1] = R - T;
-		if (Double.isNaN(T)) {
+		final double rho = S * S + B * C;
+		
+		if (rho < 0) {	// no real-valued eigenvalues
 			return false;
 		}
+		
+		final double T = Math.sqrt(rho);
+		eVals[0] = R + T;
+		eVals[1] = R - T;
 		if (A - D > 0) {
 			eVecs[0] = new double[] { S + T, C };
 			eVecs[1] = new double[] { B, -S - T };
@@ -88,18 +104,22 @@ public class Eigensolver2x2 {
 		return true;
 	}
 	
+	/**
+	 * Returns {@code true} iff the associated matrix has real 
+	 * eigenvalues.
+	 * @return as described
+	 */
 	public boolean isReal() {
 		return isReal;
 	}
 	
 	/**
-	 * Returns a vector holding the two real eigenvalues, which are both
+	 * Returns a vector holding the two real eigenvalues. Both are
 	 * {@code NaN} if the associated matrix has no real eigenvalues.
-	 * If the input matrix is symmetric, the first eigenvalue is the greater
-	 * of the two.
-	 * Note that the returned array is merely a reference to the internal array and 
-	 * should thus be used read-only!
-	 * @return as described
+	 * The first eigenvalue is the greater of the two.
+	 * Note that the method returns a reference to an internal
+	 * array and should thus be used read-only!
+	 * @return a 2-element array of eigenvalues
 	 */
 	public double[] getEigenvalues() {
 		return this.eVals;
@@ -108,17 +128,17 @@ public class Eigensolver2x2 {
 	/**
 	 * Returns a 2x2 array holding the two real eigenvectors.
 	 * When used in the form
-	 * <pre>
-	 * double[][] X = getEigenvalues(); 
-	 * </pre>
-	 * then {@code X[0]} and {@code X[1]} yield the 1st and 2nd eigenvector,
+	 * <pre>double[][] X = getEigenvalues();</pre>
+	 * then {@code X[0]} and {@code X[1]} contain the 1st and 2nd eigenvector,
 	 * respectively. If the associated matrix has no real eigenvalues,
 	 * then {@code X[0]} and {@code X[1]} are both {@code null}.
 	 * Otherwise {@code X[k][0]} and {@code X[k][1]} are the x/y components
 	 * of the k-th eigenvector.
 	 * The ordering of the returned eigenvectors is the same as for the
 	 * eigenvalues returned by {@link #getEigenvalues()}.
-	 * @return as described
+	 * Note that the method returns a reference to an internal
+	 * array and should thus be used read-only!
+	 * @return a 2-element array of 2D eigenvectors
 	 */
 	public double[][] getEigenvectors() {
 		return this.eVecs;
@@ -126,20 +146,19 @@ public class Eigensolver2x2 {
 		
 	// for Testing only --------------------------------------------------------------
 	
-	static void checkEigen(double[][] M) {
+	private static void checkEigen(double[][] M) {
 		System.out.format("Checking M  = \n%s\n", Matrix.toString(M));
 		Eigensolver2x2 es = new Eigensolver2x2(M);
 		double[] eVals = es.getEigenvalues();
 		double[][] eVecs = es.getEigenvectors();
 		
 		for (int i = 0; i < 2; i++) {
-//			System.out.format("  Eigenpair %d:\n", (i+1));
 			checkEigenPair(i+1, M, eVals[i], eVecs[i]);
 		}
 		System.out.println();
 	}
 	
-	static void checkEigenPair(int i, double[][] M, double lambda, double[] x) {
+	private static void checkEigenPair(int i, double[][] M, double lambda, double[] x) {
 		double[] Mx = Matrix.multiply(M, x);
 		double[] lx = Matrix.multiply(lambda, x);
 		double d = Matrix.normL2(Matrix.add(Mx, Matrix.multiply(-1, lx)));
@@ -154,71 +173,71 @@ public class Eigensolver2x2 {
 	}
 	
 	
-	public static void main(String[] args) {
-		PrintPrecision.set(6);
-		{
-			double[][] A = {
-					{3, -2},
-					{-4, 1}};
-			checkEigen(A);
-			// Mathematica: {5, -1} | {{-1, 1}, {1, 2}}
-		}
-		
-		{
-			double[][] A = {
-					{-0.009562, 0.011933}, 
-					{0.011933, -0.021158}};
-			checkEigen(A);
-			// Mathematica: {-0.028627, -0.002093} | {{-0.530554, 0.847651}, {-0.847651, -0.530554}}
-		}
-
-		{
-			double[][] A = {
-					{-0.004710, -0.006970},
-					{-0.006970, -0.029195}};
-			checkEigen(A);
-			// Mathematica: {-0.0310401, -0.00286493} | {{0.255902, 0.966703}, {-0.966703, 0.255902}}
-		}
-		
-		{
-			double[][] A = {
-					{0, 0},
-					{0, 1}};
-			checkEigen(A);
-			// Mathematica: {1, 0} | {{0, 1}, {1, 0}}
-		}
-		
-		{
-			double[][] A = {
-					{1, 0},
-					{0, 0}};
-			checkEigen(A);
-			// Mathematica: {1, 0} | {{1, 0}, {0, 1}}
-		}
-		
-		{	// Case 3.1
-			double[][] A = {
-					{1, 0},
-					{-2, 1}};
-			checkEigen(A);
-			// Mathematica: {1, 1} | {{0, 1}, {0, 0}} !!!!
-		}
-		
-		{	// Case 3.2
-			double[][] A = {
-					{1, -2},
-					{0, 1}};
-			checkEigen(A);
-			// Mathematica: {1, 1} | {{1, 0}, {0, 0}} !!!!
-		}
-		{	// Case 3.3
-			double[][] A = {
-					{1, 2},
-					{2, 1}};
-			checkEigen(A);
-			// Mathematica: {3, -1} | {{1, 1}, {-1, 1}}
-		}
-	}
+//	public static void main(String[] args) {
+//		PrintPrecision.set(6);
+//		{
+//			double[][] M = {
+//					{3, -2},
+//					{-4, 1}};
+//			checkEigen(M);
+//			// Mathematica: {5, -1} | {{-1, 1}, {1, 2}}
+//		}
+//		
+//		{
+//			double[][] M = {
+//					{-0.009562, 0.011933}, 
+//					{0.011933, -0.021158}};
+//			checkEigen(M);
+//			// Mathematica: {-0.028627, -0.002093} | {{-0.530554, 0.847651}, {-0.847651, -0.530554}}
+//		}
+//
+//		{
+//			double[][] M = {
+//					{-0.004710, -0.006970},
+//					{-0.006970, -0.029195}};
+//			checkEigen(M);
+//			// Mathematica: {-0.0310401, -0.00286493} | {{0.255902, 0.966703}, {-0.966703, 0.255902}}
+//		}
+//		
+//		{
+//			double[][] M = {
+//					{0, 0},
+//					{0, 1}};
+//			checkEigen(M);
+//			// Mathematica: {1, 0} | {{0, 1}, {1, 0}}
+//		}
+//		
+//		{
+//			double[][] M = {
+//					{1, 0},
+//					{0, 0}};
+//			checkEigen(M);
+//			// Mathematica: {1, 0} | {{1, 0}, {0, 1}}
+//		}
+//		
+//		{	// Case 3.1
+//			double[][] M = {
+//					{1, 0},
+//					{-2, 1}};
+//			checkEigen(M);
+//			// Mathematica: {1, 1} | {{0, 1}, {0, 0}} !!!!
+//		}
+//		
+//		{	// Case 3.2
+//			double[][] M = {
+//					{1, -2},
+//					{0, 1}};
+//			checkEigen(M);
+//			// Mathematica: {1, 1} | {{1, 0}, {0, 0}} !!!!
+//		}
+//		{	// Case 3.3
+//			double[][] M = {
+//					{1, 2},
+//					{2, 1}};
+//			checkEigen(M);
+//			// Mathematica: {3, -1} | {{1, 1}, {-1, 1}}
+//		}
+//	}
 }
 	
 /*
