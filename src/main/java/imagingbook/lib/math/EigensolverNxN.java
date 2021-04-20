53671cfed1b78de711e448c0a7b666908a156aa9
imagingbook <wilhelm.burger@fh-hagenberg.at>
2021-04-20 12:12:35 +0200

    finished EigensolverNxN

--- src/main/java/imagingbook/lib/math/EigensolverNxN.java
+++ src/main/java/imagingbook/lib/math/EigensolverNxN.java
@@ -1,6 +1,9 @@
 package imagingbook.lib.math;
 
+import static imagingbook.lib.math.Arithmetic.sqr;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -9,7 +12,15 @@ import org.apache.commons.math3.linear.EigenDecomposition;
 import org.apache.commons.math3.linear.MatrixUtils;
 
 /**
+ * Implements the calculation of eigenvalues and eigenvectors on
+ * square matrices of arbitrary size.
+ * Eigenvalues are sorted by magnitude (in descending order).
+ * A matrix may have complex eigenvalues but only real eigenvalues (and their
+ * associated eigenvectors) are considered.
+ * Uses class {@link #EigenDecomposition} of the Apache Commons Math (3) library.
+ * 
  * @author WB
+ * @version 2021/04/20
  *
  */
 public class EigensolverNxN implements Eigensolver {
@@ -23,52 +34,143 @@ public class EigensolverNxN implements Eigensolver {
 			throw new IllegalArgumentException("matrix M must be square");
 		}
 		this.ed = new EigenDecomposition(MatrixUtils.createRealMatrix(M));
-		this.eVals = ed.getRealEigenvalues();
-		this.permutation = sortArray(eVals);
+		this.eVals = new double[M.length];
+		this.permutation = sortEigenvalues(ed.getRealEigenvalues(), ed.getImagEigenvalues());
+		//print();
 	}
-
-	@Override
-	public double[] getEigenvalues() {
-		return eVals;
-	}
-
-	@Override
-	public double getEigenvalue(int k) {
-		return eVals[k];
-	}
-
-	@Override
-	public double[] getEigenvector(int k) {
-		return ed.getEigenvector(permutation[k]).toArray();
-	}
-
+	
+	/**
+	 * Returns {@code true} iff all eigenvalues of the associated matrix are real.
+	 * @return as described
+	 */
 	@Override
 	public boolean isReal() {
 		return !ed.hasComplexEigenvalues();
 	}
 
-	// Sorts vals (in descending order) and returns the associated permutation
-	// (with help from https://stackoverflow.com/a/11998394).
-	private int[] sortArray(double[] vals) {
-		List<Integer> indices = new ArrayList<>(vals.length);
-		for (int i = 0; i < vals.length; i++) {
+	/**
+	 * Returns a vector of eigenvalues, sorted by magnitude (in descending order).
+	 * {@code Double.Nan} is inserted for complex eigenvalues.
+	 * Note that the method returns a reference to an internal
+	 * array and thus results should be used read-only!
+	 * @return an array of eigenvalues
+	 */
+	@Override
+	public double[] getEigenvalues() {
+		return eVals;
+	}
+
+	/**
+	 * Returns the kth eigenvalue (lambda_k, k = 0, 1).
+	 * Eigenvalues are sorted by magnitude (in descending order).
+	 * {@code Double.Nan} is returned if the associated eigenvalue
+	 * is complex-valued (non-real).
+	 * @param k index 
+	 * @return the kth eigenvalue
+	 */
+	@Override
+	public double getEigenvalue(int k) {
+		return eVals[k];
+	}
+
+	/**
+	 * Returns the kth eigenvector.
+	 * The ordering of the returned eigenvectors is the same as for the
+	 * eigenvalues returned by {@link #getEigenvalues()}.
+	 * @param k index
+	 * @return the kth eigenvector
+	 */
+	@Override
+	public double[] getEigenvector(int k) {
+		return ed.getEigenvector(permutation[k]).toArray();
+	}
+	
+	// -----------------------------------------------------
+	
+	/**
+	 * Sorts eigenvalues by magnitude (in descending order, including complex eigenvalues),
+	 * inserts sorted values into {@link #eVals} (NaN for complex eigenvalues),
+	 * and returns the associated permutation array.
+	 * @param re real parts of all eigenvalues
+	 * @param im imaginary parts of all eigenvalues
+	 * @return the permutation array for sorting by magnitude
+	 */
+	private int[] sortEigenvalues(double[] re, double[] im) {
+		final int n = re.length;
+		double[] mag = new double[n];
+		for (int k = 0; k < n; k++) {
+			mag[k] = Math.sqrt(sqr(re[k]) + sqr(im[k]));
+		}
+		
+		// inspired by https://stackoverflow.com/a/11998394
+		List<Integer> indices = new ArrayList<>(mag.length);
+		for (int i = 0; i < mag.length; i++) {
 			indices.add(i);
 		}
+		
 		Comparator<Integer> comparator = new Comparator<Integer>() {
 			@Override
 			public int compare(Integer a, Integer b) {
-				return Double.compare(vals[b], vals[a]);
+				return Double.compare(mag[b], mag[a]);
 			}
 		};
 
 		Collections.sort(indices, comparator);
 		int[] perm = indices.stream().mapToInt(Integer::intValue).toArray();
-		double[] valsC = vals.clone();
-		for (int i = 0; i < vals.length; i++) {
-			vals[i] = valsC[perm[i]];
+		
+		// Rearrange 'eVals' and replace complex eigenvalues by NaN:
+		for (int k = 0; k < n; k++) {
+			int j = perm[k];
+			eVals[k] = (Arithmetic.isZero(im[j])) ? re[j] : Double.NaN;
 		}
-
 		return perm;
 	}
+	
 
+	@SuppressWarnings("unused")
+	private void print() {
+		System.out.println("hasComplexEigenvalues = " + ed.hasComplexEigenvalues());
+		double[] evalsR = ed.getRealEigenvalues();
+		double[] evalsI = ed.getImagEigenvalues();
+		System.out.format("evalsR = %s\n", Arrays.toString(evalsR));
+		System.out.format("evalsI = %s\n", Arrays.toString(evalsI));
+		
+		for (int k = 0; k < evalsR.length; k++) {
+			double[] evecR = ed.getEigenvector(k).toArray();
+			System.out.format("%d: eval=%f | evec=%s\n", k, getEigenvalue(k), Arrays.toString(getEigenvector(k)));
+		}
+	}
+
+	// -------------------------------------------------------------------------
+	
+	public static void main(String[] args) {
+//		double[][] M = {
+//				{3, -2},
+//				{-4, 1}};
+//		double[][] M = {
+//				{5, 2, 0},
+//				{2, 5, 0},
+//				{-3, 4, 6}};
+		double[][] M = {
+				{5, 2, 0, 1},
+				{2, 5, 0, 7},
+				{-3, 4, 6, 0},
+				{1 , 2, 3, 4}};
+		
+		EigensolverNxN solver = new EigensolverNxN(M);	
+
+		System.out.println("isReal = " + solver.isReal());
+		double[] eigenvals = solver.getEigenvalues();
+		System.out.println("evals = " + Arrays.toString(eigenvals));
+		for (int k = 0; k < eigenvals.length; k++) {
+			double lambda = eigenvals[k];
+			if (Double.isFinite(lambda)) {
+				double[] x = solver.getEigenvector(k);
+				System.out.format("λ_%d = %s\n", k, lambda);
+				System.out.format("x_%d = %s\n", k, Arrays.toString(x));
+				System.out.format("   M*x = %s\n", Arrays.toString(Matrix.multiply(M, x)));
+				System.out.format("   λ*M = %s\n", Arrays.toString(Matrix.multiply(lambda, x)));
+			}
+		}
+	}
 }
--- src/test/java/imagingbook/lib/math/EigensolverNxNTest.java
+++ src/test/java/imagingbook/lib/math/EigensolverNxNTest.java
@@ -1,13 +1,15 @@
 package imagingbook.lib.math;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class EigensolverNxNTest {
 
 	@Test
-	public void testEigensolverNxNA() {
+	public void testEigensolverNxNa() {
 		double[][] M = {
 				{3, -2},
 				{-4, 1}};
@@ -15,7 +17,7 @@ public class EigensolverNxNTest {
 	}
 	
 	@Test
-	public void testEigensolverNxNB() {
+	public void testEigensolverNxNb() {
 		double[][] M = {
 				{-0.004710, -0.006970},
 				{-0.006970, -0.029195}};
@@ -23,7 +25,7 @@ public class EigensolverNxNTest {
 	}
 	
 	@Test
-	public void testEigensolverNxNC() {
+	public void testEigensolverNxNc() {
 		double[][] M = {
 				{0, 0},
 				{0, 1}};
@@ -31,7 +33,7 @@ public class EigensolverNxNTest {
 	}
 	
 	@Test
-	public void testEigensolverNxND() {
+	public void testEigensolverNxNd() {
 		double[][] M = {
 				{1, 0},
 				{0, 0}};
@@ -39,7 +41,7 @@ public class EigensolverNxNTest {
 	}
 	
 	@Test
-	public void testEigensolverNxNE() {
+	public void testEigensolverNxNe() {
 		double[][] M = {
 				{1, 0},
 				{-2, 1}};
@@ -47,7 +49,7 @@ public class EigensolverNxNTest {
 	}
 	
 	@Test
-	public void testEigensolverNxNF() {
+	public void testEigensolverNxNf() {
 		double[][] M = {
 				{1, -2},
 				{0, 1}};
@@ -55,7 +57,7 @@ public class EigensolverNxNTest {
 	}
 	
 	@Test
-	public void testEigensolverNxNG() {
+	public void testEigensolverNxNg() {
 		double[][] M = {
 				{1, 2},
 				{2, 1}};
@@ -63,29 +65,57 @@ public class EigensolverNxNTest {
 	}
 	
 	@Test
-	public void testEigensolverNxNH() {
+	public void testEigensolverNxNh() {
 		double[][] M = {
 				{5, 2, 0},
 				{2, 5, 0},
 				{-3, 4, 6}};
 		runTest(M);
 	}
-
+	
+	@Test
+	public void testEigensolverNxNi() {
+		double[][] M = {		// has complex eigenvalues!
+				{5, 2, 0, 1},
+				{2, 5, 0, 7},
+				{-3, 4, 6, 0},
+				{1 , 2, 3, 4}};
+		runTest(M, false);
+	}
+	
+	// ---------------------------------------------------------
 	
 	private void runTest(double[][] M) {
+		runTest(M, true);
+	}
+
+	private void runTest(double[][] M, boolean shouldBeReal) {
 		EigensolverNxN solver = new EigensolverNxN(M);	
-		assertTrue(solver.isReal());
+		if (shouldBeReal)
+			assertTrue(solver.isReal());
+		else
+			assertFalse(solver.isReal());
+		
 		double[] eigenvals = solver.getEigenvalues();
 		
 		//System.out.println("eigenvals = " + Matrix.toString(eigenvals));
 		
-		for (int k = 1; k < eigenvals.length; k++) {
-			assertTrue(eigenvals[k-1] >= eigenvals[k]);
+		for (int k = 0; k < eigenvals.length - 1; k++) {
+			if (Double.isNaN(eigenvals[k])) {
+				continue;
+			}
+			for (int j = k + 1; j < eigenvals.length; j++) {
+				if (!Double.isNaN(eigenvals[j])) {
+					assertTrue(Math.abs(eigenvals[k]) >= Math.abs(eigenvals[j]));
+				}
+			}
 		}
 		
-		assertTrue(Matrix.normL2(eigenvals) > 0.01);
-		
 		for (int k = 0; k < eigenvals.length; k++) {
+			if (Double.isNaN(eigenvals[k])) {
+				continue;
+			}
+			//System.out.println("testing " + eigenvals[k]);
 			double lambda = eigenvals[k];
 			double[] x = solver.getEigenvector(k);
 			// check: M * x = λ * x
