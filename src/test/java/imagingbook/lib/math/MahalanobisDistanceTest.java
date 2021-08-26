45df8c70b28f995eea2d9344e8cf7655ad949ed2
imagingbook <wilbur@ieee.org>
2021-08-26 19:48:05 +0200

    added ChiSquaredDistance (experimental)

---
+++ src/test/java/imagingbook/lib/math/MahalanobisDistanceTest.java
@@ -0,0 +1,18 @@
+package imagingbook.lib.math;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * TODO: insert 'MahalanobisTest1' and 'MahalanobisTest2' here
+ *
+ */
+public class MahalanobisDistanceTest {
+
+	@Test
+	public void test1() {
+		// fail("Not yet implemented");
+	}
+
+}
