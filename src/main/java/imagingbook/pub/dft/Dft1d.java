0141bd4ab1d035084e5cc928952747b1f921817b
imagingbook <mail.wilhelm.burger@gmail.com>
2021-09-18 21:16:42 +0200

    added new methods

--- src/main/java/imagingbook/pub/dft/Dft1d.java
+++ src/main/java/imagingbook/pub/dft/Dft1d.java
@@ -38,6 +38,29 @@ public interface Dft1d {
 				throw new IllegalArgumentException("arrays for real/imagingary parts must be of size " + n);
 		}
 		
+		default void checkSize(float[] re, float[] im) {
+			if (re.length != im.length)
+				throw new IllegalArgumentException("arrays for real/imagingary parts must be of same size");
+		}
+		
+		/**
+		 * Calculates and returns the magnitude of the supplied complex-valued 1D data.
+		 * @param re the real part of the data
+		 * @param im the imaginary part of the data
+		 * @return a 1D array of magnitude values
+		 */
+		default float[] getMagnitude(float[] re, float[] im) {
+			checkSize(re, im);
+			final int n = re.length;
+			float[] mag = new float[n];
+			for (int i = 0; i < n; i++) {
+				float a = re[i];
+				float b = im[i];
+				mag[i] = (float) Math.sqrt(a*a + b*b);
+			}
+			return mag;
+		}
+		
 	}
 	
 	public interface Double extends Dft1d {
@@ -72,6 +95,29 @@ public interface Dft1d {
 		default void checkSize(double[] re, double[] im, int n) {
 			if (re.length != n || im.length != n)
 				throw new IllegalArgumentException("arrays for real/imagingary parts must be of size " + n);
+		}
+		
+		default void checkSize(double[] re, double[] im) {
+			if (re.length != im.length)
+				throw new IllegalArgumentException("arrays for real/imagingary parts must be of same size");
+		}
+		
+		/**
+		 * Calculates and returns the magnitude of the supplied complex-valued 1D data.
+		 * @param re the real part of the data
+		 * @param im the imaginary part of the data
+		 * @return a 1D array of magnitude values
+		 */
+		default double[] getMagnitude(double[] re, double[] im) {
+			checkSize(re, im);
+			final int n = re.length;
+			double[] mag = new double[n];
+			for (int i = 0; i < n; i++) {
+				double a = re[i];
+				double b = im[i];
+				mag[i] = (double) Math.sqrt(a*a + b*b);
+			}
+			return mag;
 		}
 	}
 	
